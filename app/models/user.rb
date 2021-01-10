a53af390a41351d63639147b9977cf4210479938
tetsu1026 <tetesu.w1026@gmail.com>
2021-01-10 16:50:03 +0900

    validatesの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  #validates :password, format: { with: VALID_PASSWORD_REGEX }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
   validates :nickname,         presence: true
-  validates :first_name_kanji, presence: true
-  validates :last_name_kanji,  presence: true
-  validates :first_name_kana,  presence: true
-  validates :last_name_kana,   presence: true
   validates :birth_day,        presence: true
 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :first_name_kanji, presence: true
+    validates :last_name_kanji,  presence: true
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :first_name_kana,  presence: true
+    validates :last_name_kana,   presence: true
+  end
+   
   has_many :items
   has_many :orders
 
