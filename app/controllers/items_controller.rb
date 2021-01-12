a6867bc02752913b8a6460cfb58b1bb7345b6665
tetsu1026 <tetesu.w1026@gmail.com>
2021-01-12 20:13:34 +0900

    Active Hashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :explanation, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :explanation, :price, :image, :category_id, :item_state_id, :delivery_fee_id, :prefecture_id, :delivery_days_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,20 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one :order
-  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :category
+    belongs_to_active_hash :item_state
+    belongs_to_active_hash :delivery_fee
+    belongs_to_active_hash :prefecture
+    belongs_to_active_hash :delivery_day
+  
+    belongs_to :user
+    has_one :order
+    has_one_attached :image
+
+    with_options numericality: { other_than: 1 } do 
+      validates :category_id
+      validates :item_state_id
+      validates :delivery_fee_id
+      validates :prefecture_id
+      validates :delivery_days_id
+    end
 end
