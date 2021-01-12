300066f1939def6a4631fa6b5201ab44c69b1786
tetsu1026 <tetesu.w1026@gmail.com>
2021-01-12 14:09:58 +0900

    itemコントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,8 +4,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
   end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :explanation, :price)
+    end
 end
