bef1ae096a158f7c55826fa0b903efe739b57565
tetsu1026 <tetesu.w1026@gmail.com>
2021-01-13 17:00:20 +0900

    不要ファイルの削除/item modelのテストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -73,6 +73,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
+      it "半角英数混合では登録できないこと" do
+        @item.price = "aaa1111"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it '半角英語だけでは登録できないこと' do
+        @item.price = "aaaaaaaaaa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
       it 'priceが300以下の時は出品できない' do
         @item.price = 299
         @item.valid?
