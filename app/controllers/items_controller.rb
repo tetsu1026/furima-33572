626741ea6933871b52f0f0da222f3adc57479ca4
tetsu1026 <tetesu.w1026@gmail.com>
2021-01-14 18:21:12 +0900

    move_to_indexの定義/viewの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :move_to_index, only: :edit
 
   def index
     @item = Item.includes(:user).order("created_at DESC")
@@ -23,9 +24,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id]) 
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+         redirect_to item_path
+    else
+       render :edit
+    end
   end
 
   private
@@ -33,4 +41,11 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :explanation, :price, :image, :category_id, :item_state_id, :delivery_fee_id, :prefecture_id, :delivery_day_id).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end 
 end
