0cea809817e28521bd2fa8c7d45f442a8060905b
tetsu1026 <tetesu.w1026@gmail.com>
2021-01-14 19:53:01 +0900

    削除が失敗した際に詳細ページに戻る処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-         redirect_to item_path
+      redirect_to item_path
     else
        render :edit
     end
@@ -37,6 +37,8 @@ class ItemsController < ApplicationController
   def destroy
     if @item.destroy
       redirect_to root_path
+    else
+      render :show
     end
   end
 
