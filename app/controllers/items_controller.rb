e050bc18d4c27552ca436a1ad660300afbad3f43
tetsu1026 <tetesu.w1026@gmail.com>
2021-01-14 19:02:05 +0900

    before_actionで処理をまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :authenticate_user!, only: :new
   before_action :move_to_index, only: :edit
 
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id]) 
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
          redirect_to item_path
     else
@@ -42,8 +40,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :explanation, :price, :image, :category_id, :item_state_id, :delivery_fee_id, :prefecture_id, :delivery_day_id).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id]) 
+  end
+
   def move_to_index
-    @item = Item.find(params[:id])
     unless user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
